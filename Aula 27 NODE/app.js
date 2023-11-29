c165d373db74b246e1e000836115c968c13caf22
Daniel Leaubon <leaubondaniel@gmail.com>
2023-11-29 12:18:14 -0300

    Aula 26 e 27

--- Aula 27 NODE/app.js
+++ Aula 27 NODE/app.js
@@ -8,30 +8,21 @@ const fs = require("fs");
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-var vetornomes = []
-
-if(fs.existsSync('nome.json')){
-   const dados = fs.readFileSync('nome.json','utf-8')
-   console.log(dados);
-   vetornomes = JSON.parse(dados)
-}
-
 
 app.get("/", (request, response) => {
     resultado = ''
     response.render("index", {resultado})
     });
  app.post("/obrigado", (request, response) => {
+    
     let nomeNoForm = request.body.nome
     let cadastro = {nome: nomeNoForm}
-    vetornomes.push(cadastro)
-    fs.writeFileSync('nome.json',`\n${JSON.stringify(vetornomes)}`)
+    fs.appendFileSync('nome.json',`\n ${JSON.stringify(cadastro)}`)
     resultado = `Olá, ${cadastro.nome}`
-    response.render("index", {cadastro});
+    response.render("index", {resultado});
  })
- app.get("/mostrar", (request, response) => {
-
-    response.render("nomes",{vetornomes});
+ app.get("/tec", (request, response) => {
+    response.render("tec");
  })
 app.use((request, response, next) => {
 response.status(404).send("<h1>Página não encontrada.</h1>");
@@ -39,3 +30,4 @@ response.status(404).send("<h1>Página não encontrada.</h1>");
 
 app.listen(port, () => {
 console.log(`Servidor funcionando na porta: ${port}`)});
+console.log(`Servidor funcionando na porta: ${port}`)
